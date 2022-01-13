29c7ad1ced1dfc271b4ab63474900b6bee342cfa
gabe <gabevenberg@gmail.com>
2022-01-13 15:54:00 -0600

    User can now compile site with custom stylesheet.
    fixes #7

--- tests/test.sh
+++ tests/test.sh
@@ -14,3 +14,18 @@ if [ "$result" = "$control" ]; then
 else
 	echo test did not pass!!!!!
 fi
+rm -r result_css
+../ASCIIsite.py --verbose --output result_css --exclude-file globignore --stylesheet test.css test
+cd result_css
+result_css=$(find . -type f -print0 | xargs -0 -P0 -n1 md5sum | sort -k 2 | md5sum)
+cd ../control_css
+control_css=$(find . -type f -print0 | xargs -0 -P0 -n1 md5sum | sort -k 2 | md5sum)
+cd ../
+tree result_css
+echo result_css sum is $result_css
+echo control_css sum is $control_css
+if [ "$result_css" = "$control_css" ]; then
+	echo test passed!
+else
+	echo test did not pass!!!!!
+fi
